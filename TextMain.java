e28b9520edf281740ba771c51ba0683c81dc0bc5
macong0503 <1512346520@qq.com>
2021-07-14 16:04:59 +0800

    Create TextMain.java

---
+++ TextMain.java
@@ -0,0 +1,8 @@
+package src.com.hqyj.mc.Text;
+
+public class TextMain {
+    public static void main(String[] args) {
+        int i = MethodText.MethodA(1, 2, 3);
+        System.out.println(i);
+    }
+}
