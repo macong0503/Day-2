971b4b061c4b2f8bb8f913617d6cb4b5cbb04a79
macong0503 <1512346520@qq.com>
2021-07-14 16:04:51 +0800

    Create MethodText_02.java

---
+++ MethodText_02.java
@@ -0,0 +1,16 @@
+package src.com.hqyj.mc.Text;
+
+public class MethodText_02 {
+    public static int add(int i,int j){
+        return i+j;
+    }
+    public static int add(int i,int j,int k){
+        return i+j+k;
+    }
+    public static double add(int i,double j){
+        return i+j;
+    }
+    public static double add(double i,int j){
+        return i+j;
+    }
+}
