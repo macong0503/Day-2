9fccee130bd12897bafb2be8afa53390611d4556
macong0503 <1512346520@qq.com>
2021-07-14 16:04:48 +0800

    Create MethodText.java

---
+++ MethodText.java
@@ -0,0 +1,11 @@
+package src.com.hqyj.mc.Text;
+
+public class MethodText {
+    public static void main(String[] args) {
+
+    }
+    public static int MethodA(int i , int j , int k){
+        return i + j + k;
+
+    }
+}
